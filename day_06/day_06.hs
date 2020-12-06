9d27279d3d6606166387f40e4b2b0595206cc224
Artemis Everfree <mail@artemis.sh>
2020-12-05 22:27:37 -0800

    day_06 - haskell - foldr1

--- day_06/day_06.hs
+++ day_06/day_06.hs
@@ -10,7 +10,7 @@ main = do
   input <- Text.readFile "input.txt"
   let groups = [Text.unpack group | group <- Text.splitOn "\n\n" input]
       part1 = sum [length (nub (sort (filter isAlpha group))) | group <- groups]
-      part2 = sum [length (foldr intersect ['a'..'z'] (lines group)) | group <- groups]
+      part2 = sum [length (foldr1 intersect (lines group)) | group <- groups]
 
   putStrLn ("Part 1: " ++ show part1)
   putStrLn ("Part 2: " ++ show part2)
